fe9e6e30ae2d571ac70b382f5e2ada9d7dd8f9e9
1Garv23 <1garv23@gmail.com>
2024-09-01 03:18:37 +0530

    ui added

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function computerChoice(){
+
     let random=Math.floor(Math.random()*3);
     if(random===0){
         return "rock";
@@ -10,38 +11,53 @@ function computerChoice(){
         return "scissor";
     }
 }
-function playerChoice(){
-    let choice=window.prompt("Enter your choice","default");
-    choice=choice.toLowerCase();
-    //if it's none of the expected values, ask user to enter it again
-    console.log(choice);
-    if(choice!="rock" && choice!="paper" && choice!="scissor"){
-        choice=playerChoice();
-    }
-    return choice;
+function removeLastResult(){
+    resultDiv.removeChild(resultPara);
 }
-let total_wins=0;
-function playGame(){
-    let player_wins=0;
-    let computer_wins=0;
-    for(let i=0; i<5; i++){
-        let comp=computerChoice();
-        let player=playerChoice();
-        if((player==="rock" && comp==="scissor")||(player==="scissor" && comp==="paper")||(player==="paper" && comp==="rock")){
-            console.log("You won the round");
+function playerChoice(event){
+    removeLastResult();
+    let choice=event.target.textContent.toLowerCase();
+    playGame(choice);
+}
+let player_wins=0;
+let computer_wins=0;
+//event delegation
+let choiceDiv=document.querySelector(".player-choice");
+document.addEventListener("click", (event)=>{playerChoice(event)});
+//for displaying result after each round
+let resultDiv=document.createElement("div");
+let resultPara=document.createElement("div");
+resultDiv.appendChild(resultPara);
+document.querySelector("body").appendChild(resultDiv);
+//for displaying the score
+let scoreDiv=document.createElement("div");
+let scorePlayer=document.createElement("p");
+let scoreComputer=document.createElement("p");
+scoreDiv.appendChild(scorePlayer);
+scoreDiv.appendChild(scoreComputer);
+document.querySelector("body").appendChild(scoreDiv);
+function playGame(player){
+    let comp=computerChoice();
+    console.log(comp);
+    let outcome="";
+    if((player==="rock" && comp==="scissor")||(player==="scissor" && comp==="paper")||(player==="paper" && comp==="rock")){
+            outcome="You won!"
             player_wins++;
-        }
-        else if((comp==="rock" && player==="scissor")||(comp==="scissor" && player==="paper")||(comp==="paper" && player==="rock")){
-            console.log("You lost the round");
+            console.log("he won");
+    }
+    else if((comp==="rock" && player==="scissor")||(comp==="scissor" && player==="paper")||(comp==="paper" && player==="rock")){
+            outcome="you lost";
             computer_wins++;
-        }
-        else{
-            console.log("Draw");
-        }
+            console.log("he lost");
     }
-    if(player_wins>computer_wins){
-        console.log("You won the match");
-        total_wins++;
-        console.log("You have won "+total_wins+" rounds");
+    else{
+            outcome="draw";
+            console.log("draw");
     }
+    resultPara.textContent=outcome;
+    resultDiv.appendChild(resultPara);
+    scorePlayer.textContent="Your wins: "+player_wins;
+    scoreComputer.textContent="Opponent wins: "+computer_wins;
 }
+
+    
